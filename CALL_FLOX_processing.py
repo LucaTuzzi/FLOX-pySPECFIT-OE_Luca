b23c2be48eb666782526bfe8f072c66ab2e80867
Luca <158610974+LucaTuzzi@users.noreply.github.com>
2026-03-29 19:01:40 +0200

    Add files via upload

--- CALL_FLOX_processing.py
+++ CALL_FLOX_processing.py
@@ -1,11 +1,23 @@
 import os
-from src.FLOX_processing_master import FLOX_processing_master
 
+#from src.AIRFLOX_processing_master import AIRFLOX_processing_master
+from src.FLOX_processing_master import FLOX_processing_master
+#from src.FLOX_processing import FLOX_processing
+
+
+#only variable to be modified by the user. The path should be relative to the script directory, 
+#and should contain the subfolders with the data to be processed #(e.g., "test-data")
+data_folder=  "Version_3.1_test-data" 
+#data_folder = r"G:\Il mio Drive\Data\2025_FRM4FLUO\Data\Storage-JB\SECOND_CAMPAIGN\SPECTROMETERS_DATA\VERSION_2\AIRFLOX DATA\UNIMIB"
 
 if __name__ == "__main__":
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    data_path = f"{script_dir}/test_tds"
-    uncertainty = f"{script_dir}/auxiliar_mat_files/uncertainty_FLOX_v2.mat"
-    cov = f"{script_dir}/auxiliar_mat_files\L2RM-AUX-V1-SIF_RHO_prior.nc"
+    #data_path = os.path.join(script_dir, "test_tds")
+    data_path = os.path.join(script_dir, data_folder)
+    uncertainty_path = data_path #path, for csv files with an absolute uncertainty for each spectrum
+    cov = os.path.join(script_dir, "auxiliar_mat_files", "L2RM-AUX-V1-SIF_RHO_prior.nc")
 
-    FLOX_processing_master(data_path, uncertainty, cov)
+    short_case = False #for testing purposes, if True it run only a subset of the data
+    FLOX_processing_master(data_path, uncertainty_path, cov, short_case)
+else:
+    print("This script is meant to be run as the main program. Please run it directly.")
