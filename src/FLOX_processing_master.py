f0303d4eebf21bf734dfeaa6c4060d5b6ecc7edd
Luca <158610974+LucaTuzzi@users.noreply.github.com>
2026-04-30 14:47:56 +0200

    Update FLOX_processing_master.py

--- src/FLOX_processing_master.py
+++ src/FLOX_processing_master.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from datetime import datetime
 import os
+import csv
 from scipy.io import loadmat
 from src.FLOX_functions import write_results_to_netcdf, load_inverse_covariance, write_csv_with_headers
 from src.FLOX_processing import FLOX_processing
@@ -42,6 +43,10 @@ def FLOX_processing_master(data_path, uncertainty_path, cov_path, uncertainty_as
     logf.write(f"data_path   = {data_path}\n")
     logf.write(f"uncertainty_path = {uncertainty_path}\n")
     logf.write(f"cov_path         = {cov_path}\n")
+    logf.write(f"uncertainty_as_input = {uncertainty_as_input}\n")
+    logf.write(f"SIF_unc_MC = {SIF_unc_MC}\n")
+    logf.write(f"parallel = {parallel}\n")
+    logf.write(f"short_case = {short_case}\n")  
     logf.flush()
 
     sa, xa_mean = load_inverse_covariance(cov_path)
@@ -63,15 +68,15 @@ def FLOX_processing_master(data_path, uncertainty_path, cov_path, uncertainty_as
     allr_specfit = None         # Reflectance    
     allar_unc = None            # Apparent Reflectance uncertainty 
 
-    # Search for input files: Reflectance as main data (so with uncertainty), Incoming radiance as atm fun (so no uncertainty).
-    Reflectance_pattern = os.path.join(data_path, "**", "Reflectance*FLUO*.csv")
-    Reflectance_list = sorted(glob.glob(Reflectance_pattern, recursive=True))
+    # Search for input files: AppReflectance as main data (so with uncertainty), Incoming radiance as atm fun (so no uncertainty).
+    AppReflectance_pattern = os.path.join(data_path, "**", "Reflectance*FLUO*.csv")
+    AppReflectance_list = sorted(glob.glob(AppReflectance_pattern, recursive=True))
     Lin_pattern = os.path.join(data_path, "**", "Incoming*FLUO*.csv")
     Lin_list = sorted(glob.glob(Lin_pattern, recursive=True))
 
     if uncertainty_as_input:
-        unc_reflectance_pattern = os.path.join(uncertainty_path, "**", "Uncertainty*Reflectance*FLUO*.csv")
-        unc_reflectance_list = sorted(glob.glob(unc_reflectance_pattern, recursive=True))
+        unc_AppReflectance_pattern = os.path.join(uncertainty_path, "**", "Uncertainty*Reflectance*FLUO*.csv")
+        unc_AppReflectance_list = sorted(glob.glob(unc_AppReflectance_pattern, recursive=True))
     else:
         unc_data = loadmat(uncertainty_path)
 
@@ -79,26 +84,26 @@ def FLOX_processing_master(data_path, uncertainty_path, cov_path, uncertainty_as
     if short_case:
         start_temp=0
         end_temp=2
-        Reflectance_list = Reflectance_list[start_temp:end_temp]
+        AppReflectance_list = AppReflectance_list[start_temp:end_temp]
         Lin_list= Lin_list[start_temp:end_temp]
         if uncertainty_as_input:
-            unc_reflectance_list = unc_reflectance_list[start_temp:end_temp] 
+            unc_AppReflectance_list = unc_AppReflectance_list[start_temp:end_temp] 
         
 
-    n_tables = len(Reflectance_list)
+    n_tables = len(AppReflectance_list)
 
     # Basic check confirming that the number of files matches:
     if uncertainty_as_input:
-        valid = (len(Lin_list) == len(Reflectance_list) == len(unc_reflectance_list))
+        valid = (len(Lin_list) == len(AppReflectance_list) == len(unc_AppReflectance_list))
     else:
-        valid = (len(Lin_list) == len(Reflectance_list))
+        valid = (len(Lin_list) == len(AppReflectance_list))
 
     if not valid:
         msg = (
             "Warning: mismatch in number of files:\n"
             f"Incoming FLUO: {len(Lin_list)}\n"
-            f"Reflectance FLUO: {len(Reflectance_list)}\n"
-            + (f"Uncertainty Reflectance FLUO: {len(unc_reflectance_list)}\n" if uncertainty_as_input else "" )
+            f"AppReflectance FLUO: {len(AppReflectance_list)}\n"
+            + (f"Uncertainty AppReflectance FLUO: {len(unc_AppReflectance_list)}\n" if uncertainty_as_input else "" )
         )
         logf.write(msg + "\n")
         logf.flush()
@@ -110,37 +115,45 @@ def FLOX_processing_master(data_path, uncertainty_path, cov_path, uncertainty_as
         logf.write(f"\nProcessing file {i_pair+1} of {n_tables}\n")
         logf.flush()
 
-        # Read the Reflectance CSV
-        fname_Reflectance = Reflectance_list[i_pair]
-        logf.write(f"Reflectance: {fname_Reflectance}\n")
-        data_Reflectance = pd.read_csv(fname_Reflectance, sep=";", header=0)
-        Reflectance_table = data_Reflectance.iloc[:, 1:].to_numpy()
+        # Read the AppReflectance CSV
+        fname_AppReflectance = AppReflectance_list[i_pair]
+        logf.write(f"AppReflectance: {fname_AppReflectance}\n")
+        with open(fname_AppReflectance, "r", encoding="utf-8") as f:
+            sample = "".join([f.readline() for _ in range(2)])
+            delimiter = csv.Sniffer().sniff(sample, delimiters=";,").delimiter
+        data_AppReflectance = pd.read_csv(fname_AppReflectance,sep=delimiter,header=0)
+        AppReflectance_table = data_AppReflectance.iloc[:, 1:].to_numpy()
 
         # Read the Incoming CSV
         fname_Lin = Lin_list[i_pair]
         logf.write(f"Incoming: {fname_Lin}\n")
-        data_Lin = pd.read_csv(fname_Lin, sep=";", header=0)
+        with open(fname_Lin, "r", encoding="utf-8") as f:
+            sample = "".join([f.readline() for _ in range(2)])
+            delimiter = csv.Sniffer().sniff(sample, delimiters=";,").delimiter
+        data_Lin = pd.read_csv(fname_Lin, sep=delimiter, header=0)
         Lin_table = data_Lin.iloc[:, 1:].to_numpy()
 
-        # Read the Reflectance Uncertainty CSV
+        # Read the AppReflectance Uncertainty CSV
         if uncertainty_as_input:
-            fname_unc_Reflectance = unc_reflectance_list[i_pair]
-            logf.write(f"Uncertainty: {fname_unc_Reflectance}\n")
-            data_unc_Reflectance = pd.read_csv(fname_unc_Reflectance, sep=";", header=0)
-            unc_Reflectance_table = data_unc_Reflectance.iloc[:, 1:].to_numpy()
+            fname_unc_AppReflectance = unc_AppReflectance_list[i_pair]
+            logf.write(f"Uncertainty: {fname_unc_AppReflectance}\n")
+            with open(fname_unc_AppReflectance, "r", encoding="utf-8") as f:
+                sample = "".join([f.readline() for _ in range(2)])
+                delimiter = csv.Sniffer().sniff(sample, delimiters=";,").delimiter
+            data_unc_AppReflectance = pd.read_csv(fname_unc_AppReflectance, sep=delimiter, header=0)
+            unc_AppReflectance_table = data_unc_AppReflectance.iloc[:, 1:].to_numpy()
         else:
-            logf.write(f"Uncertainty: to be computed\n")
-        
-        
+            logf.write(f"Uncertainty: computed\n")
+
         #!!! Check consistency of file names: assume that the last 6 digits before .csv must be equal
-        base_name_Reflectance = os.path.basename(fname_Reflectance)[-10:-4]  # Remove .csv extension
+        base_name_AppReflectance = os.path.basename(fname_AppReflectance)[-10:-4]  # Remove .csv extension
         base_name_Lin = os.path.basename(fname_Lin)[-10:-4]
 
         if uncertainty_as_input:
-            base_name_unc_Reflectance = os.path.basename(fname_unc_Reflectance)[-10:-4]
-            consistent = (base_name_Reflectance == base_name_unc_Reflectance == base_name_Lin)
+            base_name_unc_AppReflectance = os.path.basename(fname_unc_AppReflectance)[-10:-4]
+            consistent = (base_name_AppReflectance == base_name_unc_AppReflectance == base_name_Lin)
         else:
-            consistent = (base_name_Reflectance == base_name_Lin)
+            consistent = (base_name_AppReflectance == base_name_Lin)
 
         if not consistent:
             msg = f"Warning: Inconsistent file names for pair {i_pair+1}"
@@ -149,10 +162,10 @@ def FLOX_processing_master(data_path, uncertainty_path, cov_path, uncertainty_as
             raise ValueError(msg)
 
         #get only once:
-        # #1) (from Reflectance), assuming all files have the same wavelength (first column) and UTC timestamp (first row)
+        # #1) (from AppReflectance), assuming all files have the same wavelength (first column) and UTC timestamp (first row)
         # 2) (from uncertainty file, if not given as input) get the uncertainty values for the wavelength grid of the input spectra, to be used as input in the retrieval
-        wvl_qepro = data_Reflectance.iloc[:, 0].to_numpy()
-        utc_column = data_Reflectance.columns[1:]
+        wvl_qepro = data_AppReflectance.iloc[:, 0].to_numpy()
+        utc_column = data_AppReflectance.columns[1:]
 
         # Wavelength Definition
         lb = np.argmin(np.abs(wvl_qepro - wvlRet[0]))
@@ -179,23 +192,33 @@ def FLOX_processing_master(data_path, uncertainty_path, cov_path, uncertainty_as
 
         # Spectral subset of input spectra to min_wvl - max_wvl range;  # Replace NaN and inf values with zeros (or a small number) to avoid issues in processing
         #!!! is it right to fix to 0 ?
-        Reflectance = Reflectance_table[lb:ub+1, :]
-        Reflectance = np.nan_to_num(Reflectance, nan=0.0, posinf=0.0, neginf=0.0) 
+        AppReflectance = AppReflectance_table[lb:ub+1, :]
+        AppReflectance = np.nan_to_num(AppReflectance, nan=0.0, posinf=0.0, neginf=0.0) 
         Lin = Lin_table[lb:ub+1, :] * 1e3 #convert Lin from W to mW.
         Lin = np.nan_to_num(Lin, nan=0.0, posinf=0.0, neginf=0.0)
+
+        valid_cols = np.all(AppReflectance >= 0, axis=0)
+        logf.write(f"Spectra not processed (AppReflectance<0), indices : ~valid_cols\n")
+        logf.flush()
+        
+        AppReflectance = AppReflectance[:, valid_cols]
+        Lin = Lin[:, valid_cols]
+
         if uncertainty_as_input:
-            unc_Reflectance = unc_Reflectance_table[lb:ub+1, :]
+            unc_AppReflectance = unc_AppReflectance_table[lb:ub+1, :]
+            unc_AppReflectance = unc_AppReflectance[:, valid_cols]
         else:
             Lin_unc = flox_unc_down[:, np.newaxis] * Lin
-            Lout_unc = flox_unc_up[:, np.newaxis] * Lin * Reflectance
-            #this is the relative unc propagation forumla, using only reflectance and Lin, so Lout = Rreflectance * Lin
-            unc_Reflectance = (np.sqrt(Lout_unc**2 + (Reflectance * Lin_unc)**2 ))/Lin
+            Lout_unc = flox_unc_up[:, np.newaxis] * Lin * AppReflectance
+            #this is the relative unc propagation forumla, using only AppReflectance and Lin, so Lout = AppReflectance * Lin
+            unc_AppReflectance = (np.sqrt(Lout_unc**2 + (AppReflectance * Lin_unc)**2 ))/Lin
         
-        unc_Reflectance = np.nan_to_num(unc_Reflectance, nan=0.0, posinf=0.0, neginf=0.0)  
+        unc_AppReflectance = np.nan_to_num(unc_AppReflectance, nan=0.0, posinf=0.0, neginf=0.0)  
 
-        # Compute UTC_time for the header information (from Reflectance)
+        # Compute UTC_time for the header information (from AppReflectance)
+        utc_column = utc_column[valid_cols]
         utc_time = [header.strip() for header in utc_column]
-        #folder_date = os.path.basename(os.path.dirname(fname_Reflectance))
+        #folder_date = os.path.basename(os.path.dirname(fname_AppReflectance))
 
         doy_day_frac = []
         utc_datime_str = []
@@ -232,15 +255,15 @@ def FLOX_processing_master(data_path, uncertainty_path, cov_path, uncertainty_as
             failed_spectra       # List of spectra indices that failed during retrieval
         ) = FLOX_processing(         
             wvl=wvl_sub,             # Wavelength subset for processing
-            app_ref_array=Reflectance,
-            app_ref_unc_array = unc_Reflectance,
+            app_ref_array=AppReflectance,
+            app_ref_unc_array = unc_AppReflectance,
             Lin_array=Lin ,        # Incident radiance (Lin) used only in arho_sim = rho + fluorescence / Lin
             sa=sa,  # covariance matrix  
             xa_mean=xa_mean,                # mean state vector
             SIF_unc_MC=SIF_unc_MC,
             parallel=parallel
         )
-        n_spectra = Reflectance_table.shape[1]
+        n_spectra = AppReflectance.shape[1]
 
         # Accumulate the results
         out_arr_local = []
